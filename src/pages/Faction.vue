fc6d110181c548c450043faf3d13428b0cb54e4e
Yoduh <ahandlovits@gmail.com>
2023-09-08 17:12:20 -0400

    typo

--- src/pages/Faction.vue
+++ src/pages/Faction.vue
@@ -234,7 +234,7 @@
         />
         <div>
           While you control Mecatol Rex, it gains SPACE CANNON 5 and PRODUCTION
-          3. Gain 2 command tokens when another player socres VP using Imperial.
+          3. Gain 2 command tokens when another player scores VP using Imperial.
         </div>
       </div>
     </section>
