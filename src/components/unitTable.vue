da60761febfcba3f6d3037132812c85e2109c7b3
Yoduh <ahandlovits@gmail.com>
2023-09-25 15:18:12 -0400

    added unit images

--- src/components/unitTable.vue
+++ src/components/unitTable.vue
@@ -9,32 +9,43 @@
     class="q-table q-mb-xl"
   >
     <template v-slot:top>
-      <div class="flex column">
-        <div class="row items-center">
-          <div class="text-h6">{{ unit.name }}</div>
-          <q-btn
-            v-if="unit.notes && unit.notes.length > 0"
-            @click="$emit('showNote')"
-            color="amber-4"
-            round
-            dense
-            size="12px"
-            flat
-            icon="help_outline"
-          />
-        </div>
-        <div class="text-caption text-italic">
-          {{ unit.subtype ? unit.subtype : unit.type }}
-        </div>
-        <div class="q-mb-sm" v-if="prereqs">
-          Pre-requisites:
-          <span v-for="(prereq, idx) in prereqs" :key="idx">
-            <TI4Icon
-              type="tech"
-              :name="prereq.techType"
-              :quantity="prereq?.quantity"
+      <div class="flex column col">
+        <div class="row">
+          <div class="column col-auto">
+            <q-img
+              :src="getImage(unit.type, unit.subtype, unit.name)"
+              width="50px"
+              class="q-mr-sm"
             />
-          </span>
+          </div>
+          <div class="column col-auto">
+            <div class="row items-center">
+              <div class="text-h6">{{ unit.name }}</div>
+              <q-btn
+                v-if="unit.notes && unit.notes.length > 0"
+                @click="$emit('showNote')"
+                color="amber-4"
+                round
+                dense
+                size="12px"
+                flat
+                icon="help_outline"
+              />
+            </div>
+            <div class="text-caption text-italic">
+              {{ unit.subtype ? unit.subtype : unit.type }}
+            </div>
+            <div class="q-mb-sm" v-if="prereqs">
+              Pre-requisites:
+              <span v-for="(prereq, idx) in prereqs" :key="idx">
+                <TI4Icon
+                  type="tech"
+                  :name="prereq.techType"
+                  :quantity="prereq?.quantity"
+                />
+              </span>
+            </div>
+          </div>
         </div>
         <div>
           <ul
@@ -97,6 +108,13 @@ function toTitleCase(str: string) {
   return str.replace(/\w\S*/g, function (txt) {
     return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
   });
+}
+
+function getImage(type: string, subtype: string, name: string) {
+  let genericName = name.split(' II')[0];
+  if (subtype) genericName = subtype.split(' II')[0];
+  else if (type === 'Mech' || type === 'Flagship') genericName = type;
+  return `${process.env.API_URL}/images/units/${genericName}.png`;
 }
 </script>
 
