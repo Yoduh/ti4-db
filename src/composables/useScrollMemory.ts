cc5740b2199bddacd1f4dbbc0feaeb4996b56a4b
Alex Handlovits <ahandlovits@gmail.com>
2025-11-03 13:54:03 -0500

    scrollMemory composable needed for changes in previous commit

---
+++ src/composables/useScrollMemory.ts
@@ -0,0 +1,25 @@
+import { watch } from 'vue';
+import { useRoute } from 'vue-router';
+
+/**
+ * Remembers and restores scroll position when switching between The Firmament and The Obsidian faction pages
+ */
+const scrollPositions: Record<string, number> = {};
+export function useScrollMemory(): void {
+  const route = useRoute();
+
+  watch(
+    () => route.fullPath,
+    (newPath, oldPath) => {
+      if (
+        (oldPath === '/factions/29' || oldPath === '/factions/28') &&
+        (newPath === '/factions/29' || newPath === '/factions/28')
+      ) {
+        scrollPositions[oldPath] = window.scrollY;
+        setTimeout(() => {
+          window.scrollTo({ top: scrollPositions[oldPath] });
+        }, 0);
+      }
+    }
+  );
+}
